cfa8135fe9f7f8adbe68636620ca500379143008
ahmadhabibi14 <ahmadhabibi7159@gmail.com>
2023-02-08 21:10:04 +0800

    Cleaning code

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
-## TELEGRAM BOT, Built with Go lang
+## TELEGRAM BOT with ChatGPT, Built with Go lang
 
 ### How to start ?
 
-1. Create your API token at https://t.me/BotFather
-2. Create `.env` file in the root directory
-3. Add variable named `TELEGRAM_API_TOKEN` inside of `.env` file,
-   and then put your API token as its value
-4. Run `go get` to install required modules
-5. Make sure to assign execute permission to `start.sh` file
-6. Run `start.sh`
+1. Generate your Telegram bot token at https://t.me/BotFather
+2. Generate OpenAI API key at https://platform.openai.com/account/api-keys
+3. Create `.env` file in the root directory
+4. Add variable named `TELEGRAM_BOT_TOKEN` and `OPENAI_API_KEY` inside of `.env` file,
+   and then put your Telegram bot token and OpenAI API key as its value
+5. Run `go get` to install required modules
+6. Make sure to assign execute permission to `start.sh` file
+7. Run `start.sh`
 
-### Wanna modify bot command ?
+### Wanna modify bot ?
 
 If you want, you can rebuild the whole source code 😂
--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
+	gogpt "github.com/sashabaranov/go-gpt3"
 )
 
 func init() {
+	// Initialize .env file
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env files")
@@ -16,54 +19,51 @@ func init() {
 }
 
 func main() {
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_API_TOKEN"))
+	// Create a new OpenAI API client
+	c := gogpt.NewClient(os.Getenv("OPENAI_API_KEY"))
+	ctx := context.Background()
+
+	// Create a new Telegram bot service
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_BOT_TOKEN"))
 	if err != nil {
-		panic(err)
+		log.Fatalf("Failed to create Telegram bot: %s", err)
 	}
-
 	bot.Debug = true
-
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
+	// Start listening for updates
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 30000
-
 	updates := bot.GetUpdatesChan(u)
-
 	for update := range updates {
-		// Ignore any non-message updates
+
+		// Ignore if non-message updates
 		if update.Message == nil {
 			continue
 		}
 
-		if !update.Message.IsCommand() { // Ignore any non-command Messages
+		// Use the OpenAI API client to generate a response //
+
+		// Create request model
+		req := gogpt.CompletionRequest{
+			Model:     gogpt.GPT3Davinci, /*gogpt.GPT3Ada*/
+			MaxTokens: 5,
+			Prompt:    update.Message.Text,
+		}
+		// Generate response
+		resp, err := c.CreateCompletion(ctx, req)
+		if err != nil {
+			log.Printf("Failed to generate response from OpenAI: %s", err)
 			continue
 		}
 
-		// Create a new MessageConfig. We don't have text yet.
-		// so we leave it empty
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-
-		// Extract the command from the Message
-		switch update.Message.Command() {
-		case "help":
-			msg.Text = "I understand /sayhi and /status."
-		case "sayhi":
-			msg.Text = "Hi :)"
-		case "status":
-			msg.Text = "I'm ok."
-		default:
-			msg.Text = "I don't know the command"
-		}
-
-		if _, err := bot.Send(msg); err != nil {
-			log.Panic(err)
-		}
-
+		// Send the response back to the user
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, resp.Choices[0].Text)
 		msg.ReplyToMessageID = update.Message.MessageID
-
-		if _, err := bot.Send(msg); err != nil {
-			panic(err)
+		_, err = bot.Send(msg)
+		if err != nil {
+			log.Printf("Failed to send response to user: %s", err)
+			continue
 		}
 	}
 }
