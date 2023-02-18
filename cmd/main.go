64f1a909d9c33eb4b72c9e866931b55a79e85cae
ahmadhabibi14 <ahmadhabibi7159@gmail.com>
2023-02-18 11:22:49 +0800

    Remove third party ChatGPT library

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
-	"context"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/ahmadhabibi14/go-tele-bot/models"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
-	gogpt "github.com/sashabaranov/go-gpt3"
 )
 
+// Initialize .env file
 func init() {
-	// Initialize .env file
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env files")
@@ -19,11 +23,6 @@ func init() {
 }
 
 func main() {
-	// Create a new OpenAI API client
-	c := gogpt.NewClient(os.Getenv("OPENAI_API_KEY"))
-	ctx := context.Background()
-
-	// Create a new Telegram bot service
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_BOT_TOKEN"))
 	if err != nil {
 		log.Fatalf("Failed to create Telegram bot: %s", err)
@@ -31,39 +30,44 @@ func main() {
 	bot.Debug = true
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	// Start listening for updates
 	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 30000
+	u.Timeout = 60
 	updates := bot.GetUpdatesChan(u)
 	for update := range updates {
-
-		// Ignore if non-message updates
-		if update.Message == nil {
-			continue
+		reqBody := models.Request{
+			ModelRequest: "text-davinci-003",
+			Prompt:       update.Message.Text,
+			Temperature:  1,
+			MaxTokens:    100,
 		}
-
-		// Use the OpenAI API client to generate a response //
-
-		// Create request model
-		req := gogpt.CompletionRequest{
-			Model:     gogpt.GPT3Davinci, /*gogpt.GPT3Ada*/
-			MaxTokens: 5,
-			Prompt:    update.Message.Text,
-		}
-		// Generate response
-		resp, err := c.CreateCompletion(ctx, req)
+		jsonBody, _ := json.Marshal(reqBody)
+		req, err := http.NewRequest("POST", "https://api.openai.com/v1/completions", bytes.NewBuffer(jsonBody))
+		req.Header.Add("Content-Type", "application/json")
+		req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", os.Getenv("OPENAI_API_KEY")))
 		if err != nil {
-			log.Printf("Failed to generate response from OpenAI: %s", err)
-			continue
+			panic(err)
 		}
 
-		// Send the response back to the user
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, resp.Choices[0].Text)
+		// Send the request
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+		// Read the response
+		jsonData, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+
+		var data models.TextCompletionResponse
+		json.Unmarshal([]byte(jsonData), &data)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, data.Choices[0].Text)
 		msg.ReplyToMessageID = update.Message.MessageID
-		_, err = bot.Send(msg)
-		if err != nil {
-			log.Printf("Failed to send response to user: %s", err)
-			continue
-		}
+
+		bot.Send(msg)
 	}
 }
